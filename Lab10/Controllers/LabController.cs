ac29eb594fa24a1aad8384020b160b801b201edf
Юра Микитій <yurii.mykytii-ip221@nung.edu.ua>
2024-11-12 16:50:39 +0200

    Add project files.

---
+++ Lab10/Controllers/LabController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab10.Controllers
+{
+    public class LabController : Controller
+    {
+        // GET: Lab
+        public ActionResult ListOfPeople()
+        {
+            List<Person> people = new List<Person>();
+
+            using (var db = new SurveyDBEntities())
+            {
+                people = db.Person
+                    .OrderByDescending(x => x.Age)
+                    .ThenBy(x => x.LastName)
+                    .ThenBy(x => x.FirstName)
+                    .ToList();
+            }
+
+            return View(people);
+        }
+        [HttpGet]
+        public ActionResult PersonDetails(Guid personId)
+        {
+            Person model = new Person();
+            using (var db = new SurveyDBEntities())
+            {
+                model = db.Person.Find(personId); 
+            }
+
+           
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(model);
+        }
+    }
+}
